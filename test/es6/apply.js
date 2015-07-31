962b02863d2006fbdb711325916fe5cbea6494a5
unbornchikken <gabor.mezo@outlook.com>
2015-07-31 13:53:37 +0200

    apply ok

--- test/es6/apply.js
+++ test/es6/apply.js
@@ -72,7 +72,7 @@ describe("Apply", function () {
                                             collection: "= this.stuff",
                                             toArray: true,
                                             args: {
-                                                "@applied": {
+                                                "@crossApplied": {
                                                     name: "other",
                                                     collection: "= this.otherStuff",
                                                     query: {
@@ -102,6 +102,98 @@ describe("Apply", function () {
                 assert.equal(doc.text.value, "Hello");
                 assert.equal(doc.other.str, "Hello");
                 assert(doc.other.bool);
+            })().nodeify(done);
+        });
+
+        it("should do a grouped cross apply", function (done) {
+            this.timeout(30000);
+
+            var engine = new ActivityExecutionEngine({
+                "@require": path.join(__dirname, "../../lib/" + es + "/activities"),
+                "@block": [
+                    {
+                        "@mongoDBContext": {
+                            connections: process.env.MONGO_URL,
+                            args: {
+                                "@block": {
+                                    stuff: [
+                                        {
+                                            _id: 1,
+                                            text: {
+                                                value: "Hello"
+                                            },
+                                            num: 1
+                                        },
+                                        {
+                                            _id: 2,
+                                            text: {
+                                                value: "World"
+                                            },
+                                            num: 2
+                                        },
+                                        {
+                                            _id: 3,
+                                            text: {
+                                                value: "Woot"
+                                            },
+                                            num: 3
+                                        }
+                                    ],
+                                    otherStuff: [
+                                        {
+                                            _id: 1,
+                                            str: "Hello",
+                                            bool: true
+                                        },
+                                        {
+                                            _id: 2,
+                                            str: "World",
+                                            bool: false
+                                        },
+                                        {
+                                            _id: 3,
+                                            str: "Klow",
+                                            bool: false
+                                        }
+                                    ],
+                                    args: {
+                                        "@apply": {
+                                            collection: "= this.stuff",
+                                            toArray: true,
+                                            args: {
+                                                "@crossApplied": {
+                                                    name: "other",
+                                                    collection: "= this.otherStuff",
+                                                    flat: false,
+                                                    query: {
+                                                        str: "# text.value",
+                                                        bool: {
+                                                            $ne: false
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ]
+            });
+
+            async(function*() {
+                let result = yield engine.invoke();
+                assert(_.isArray(result));
+                assert.equal(result.length, 1);
+                let doc = result[0];
+                assert.equal(doc._id, 1);
+                assert.equal(doc.num, 1);
+                assert.equal(doc.text.value, "Hello");
+                assert(_.isArray(doc.other));
+                assert.equal(doc.other.length, 1);
+                assert.equal(doc.other[0].str, "Hello");
+                assert(doc.other[0].bool);
             })().nodeify(done);
         });
     });
@@ -168,10 +260,9 @@ describe("Apply", function () {
                                             collection: "= this.stuff",
                                             toArray: true,
                                             args: {
-                                                "@applied": {
+                                                "@outerApplied": {
                                                     name: "other",
                                                     collection: "= this.otherStuff",
-                                                    type: "outer",
                                                     query: {
                                                         str: "# text.value",
                                                         bool: {
@@ -213,6 +304,122 @@ describe("Apply", function () {
                     sum += item.num;
                 }
                 assert.equal(sum, 2 + 3);
+            })().nodeify(done);
+        });
+
+        it("should do a grouped outer apply", function (done) {
+            this.timeout(30000);
+
+            var engine = new ActivityExecutionEngine({
+                "@require": path.join(__dirname, "../../lib/" + es + "/activities"),
+                "@block": [
+                    {
+                        "@mongoDBContext": {
+                            connections: process.env.MONGO_URL,
+                            args: {
+                                "@block": {
+                                    stuff: [
+                                        {
+                                            _id: 1,
+                                            text: {
+                                                value: "Hello"
+                                            },
+                                            num: 1
+                                        },
+                                        {
+                                            _id: 2,
+                                            text: {
+                                                value: "World"
+                                            },
+                                            num: 2
+                                        },
+                                        {
+                                            _id: 3,
+                                            text: {
+                                                value: "Woot"
+                                            },
+                                            num: 3
+                                        }
+                                    ],
+                                    otherStuff: [
+                                        {
+                                            _id: 1,
+                                            str: "Hello",
+                                            bool: true
+                                        },
+                                        {
+                                            _id: 2,
+                                            str: "World",
+                                            bool: false
+                                        },
+                                        {
+                                            _id: 3,
+                                            str: "Klow",
+                                            bool: false
+                                        },
+                                        {
+                                            _id: 4,
+                                            str: "Hello",
+                                            bool: true
+                                        }
+                                    ],
+                                    no: 2,
+                                    args: {
+                                        "@apply": {
+                                            collection: "= this.stuff",
+                                            toArray: true,
+                                            args: {
+                                                "@outerApplied": {
+                                                    name: "other",
+                                                    collection: "= this.otherStuff",
+                                                    flat: false,
+                                                    query: {
+                                                        str: "# text.value",
+                                                        bool: {
+                                                            $ne: false
+                                                        }
+                                                    }
+                                                }
+                                            },
+                                            transform: function(doc) {
+                                                if (doc.num !== this.no) {
+                                                    doc.other2 = doc.other;
+                                                    delete doc.other;
+                                                    return doc;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ]
+            });
+
+            async(function*() {
+                let result = yield engine.invoke();
+                assert(_.isArray(result));
+                assert.equal(result.length, 2);
+                let doc = result[0];
+                assert.equal(doc._id, 1);
+                assert.equal(doc.num, 1);
+                assert(_.isArray(doc.other2));
+                assert.equal(doc.other2.length, 2);
+                assert.equal(doc.text.value, "Hello");
+                assert.equal(doc.other2[0].str, "Hello");
+                assert(doc.other2[0].bool);
+                assert.equal(doc.other2[0]._id, 1);
+                assert.equal(doc.other2[1].str, "Hello");
+                assert(doc.other2[1].bool);
+                assert.equal(doc.other2[1]._id, 4);
+                let sum = 0;
+                for (let item of result.slice(1)) {
+                    assert(_.isArray(item.other2));
+                    assert.equal(item.other2.length, 0);
+                    sum += item.num;
+                }
+                assert.equal(sum, 3);
             })().nodeify(done);
         });
     });
